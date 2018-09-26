923ba742d9d492dbda06efab77f18335877774f0
Billy Kirchner <bkirchner27@gmail.com>
2018-09-26 15:58:58 -0500

    Form and welcome page created

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route("/welcome", methods=['POST'])
+
+def welcome():
+    user_name = request.form['user-name']
+    if len(user_name) > 20 or len(user_name) < 3:
+        user_name_error = "Username must be between 3 and 20 characters"
+        return redirect('/?user_name_error =' + user_name_error)
+    return render_template('welcome.html', user_name=user_name)
+
+@app.route("/")
+def index():
+    user_name_error = request.args.get('user_name_error')
+    create_pw_error = request.args.get('create_pw_error')
+    confirm_pw_error = request.args.get('confirm_pw_error')
+    enter_email_error = request.args.get('enter_email_error')
+
+    if not user_name_error:
+        user_name_error = ''
+    if not create_pw_error:
+        create_pw_error = ''
+    if not confirm_pw_error:
+        confirm_pw_error = ''
+    if not enter_email_error:
+        enter_email_error = ''
+    
+    #user_name = request.args.get('user-name')
+    #create_pw = request.args.get('create-pw')
+    #confirm_pw = request.args.get('confirm-pw')
+    #enter_email = request.args.get('enter-email')
+
+
+
+    return render_template('edit.html', user_name_error=user_name_error, create_pw_error=create_pw_error,
+     confirm_pw_error=confirm_pw_error, enter_email_error=enter_email_error)
+
+app.run()
