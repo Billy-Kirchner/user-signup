a307cbaa954874c772fdd5b449311d31e39eeb28
Billy Kirchner <bkirchner27@gmail.com>
2018-10-04 06:34:41 -0500

    user-signup final fixed

--- main.py
+++ main.py
@@ -14,52 +14,62 @@ def welcome():
     enter_email = request.form['enter-email']
     email_segments = enter_email.split('@', 1)
     email_dots = enter_email.split('.', 1)
-
+    user_name_error = ''
+    create_pw_error = ''
+    confirm_pw_error = ''
+    enter_email_error = ''
+    
     if user_name.strip() == '':
         user_name_error = "Please enter a username"
-        return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        #return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
 
     if ' ' in user_name:
         user_name_error = "Username cannot contain spaces"
-        return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        #return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
 
     if len(user_name) > 20 or len(user_name) < 3:
         user_name_error = "Username must be between 3 and 20 characters"
-        return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        #return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
 
     if create_pw.strip() == '':
         create_pw_error = "Please enter a password"
-        return redirect("/?create_pw_error=" + create_pw_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        #return redirect("/?create_pw_error=" + create_pw_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
 
     if ' ' in create_pw:
         create_pw_error = "Password cannot contain spaces"
-        return redirect("/?create_pw_error=" + create_pw_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        #return redirect("/?create_pw_error=" + create_pw_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
     
     if confirm_pw != create_pw:
         confirm_pw_error = "Passwords do not match"
-        return redirect("/?confirm_pw_error=" + confirm_pw_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        #return redirect("/?confirm_pw_error=" + confirm_pw_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
     
     if enter_email != '':
 
         if len(email_segments[0]) > 20 or len(email_segments[0]) < 3:
             enter_email_error = "email name must be between 3 and 20 characters"
-            return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+            #return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
     
         if ' ' in enter_email:
             enter_email_error = 'Invalid email address. Cannot contain spaces'
-            return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+            #return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
 
         if '@' not in enter_email or '.' not in enter_email:
             enter_email_error = 'Invalid email address. Should contain exactly 1 "@" and 1 "."'
-            return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+            #return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
 
-        if '@' in email_segments[1] or '.' in email_dots[1] or '@' not in enter_email or '.' not in enter_email:
+        if '@' not in enter_email or '.' not in enter_email:
             enter_email_error = 'Invalid email address. Should contain exactly 1 "@" and 1 "."'
-            return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
+        elif '@' in email_segments[1] or '.' in email_dots[1]:
+            enter_email_error = 'Invalid email address. Should contain exactly 1 "@" and 1 "."'
+            #return redirect("/?enter_email_error=" + enter_email_error + '&user_name=' + user_name + '&enter_email=' + enter_email)
     
-    user_name_escaped = cgi.escape(user_name, quote=True)
-    return render_template('welcome.html', user_name=user_name_escaped)
-
+    if user_name_error or create_pw_error or confirm_pw_error or enter_email_error:
+        return redirect("/?user_name_error=" + user_name_error + '&user_name=' + user_name + '&enter_email=' + enter_email
+                        + '&create_pw_error=' + create_pw_error + '&confirm_pw_error=' + confirm_pw_error + '&enter_email_error=' + enter_email_error)
+    else:
+        user_name_escaped = cgi.escape(user_name, quote=True)
+        return render_template('welcome.html', user_name=user_name_escaped)
+    
 @app.route("/")
 def index():
     user_name_error = request.args.get('user_name_error')
